1da8f326d9b6d1ec84fd4f7b115db1d93c306222
prasannakumarbhursu <prasannakumar.bhursu@vassarlabs.com>
2024-05-23 18:08:45 +0530

    added filter functionality

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { FC, useCallback, useEffect, useState } from "react";
 import styles from "../styles/page.module.css";
 import ShareButton from "./ShareButton";
@@ -20,17 +19,17 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
   const [error, setError] = useState("");
   const [bannerImage, setBannerImage] = useState("");
   const [errorInGetByName, setErrorInGetByName] = useState("");
-  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [selectedFilter, setSelectedFilter] = useState<string | null>("all");
   const [totalProdcuts, setTotalProducts] = useState("");
   const [filteredData, setFilteredData] = useState<any[]>([]);
 
-  const fetchProducts = useCallback(async (page: number) => {
+  const fetchProducts = useCallback(async (page: number, filters: any[] = [], isAppending: boolean = false) => {
     setLoading(true);
     const requestBody = {
       input: {
         page,
         pageSize: 10,
-        filters: [],
+        filters,
         id: "#HomeHunts",
         entity: "vibe",
       },
@@ -57,14 +56,15 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
       if ("code" in data) {
         setError(data.message);
       } else {
-        setProducts((prevProducts) => [
-          ...prevProducts,
-          ...data.data.getListingProducts.products,
-        ]);
-        {
-          totalProdcuts === "" &&
-            setTotalProducts(data.data.getListingProducts.totalProducts);
+        if (isAppending) {
+          setProducts((prevProducts) => [
+            ...prevProducts,
+            ...data.data.getListingProducts.products,
+          ]);
+        } else {
+          setProducts(data.data.getListingProducts.products);
         }
+        setTotalProducts(data.data.getListingProducts.totalProducts);
         setError("");
       }
     } catch (error: any) {
@@ -101,10 +101,7 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
       if ("code" in data) {
         setErrorInGetByName(data.message);
       } else {
-        {
-          bannerImage === "" &&
-            setBannerImage(data.data.getVibeByName.imageUrl);
-        }
+        setBannerImage(data.data.getVibeByName.imageUrl);
         setErrorInGetByName("");
       }
     } catch (error: any) {
@@ -116,16 +113,17 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
 
   useEffect(() => {
     fetchName();
-    fetchProducts(page);
-  }, [page, fetchProducts]);
+  }, [fetchName]);
+
+  useEffect(() => {
+    fetchProducts(page, selectedFilter === "all" ? [] : [{ id: selectedFilter, type: "CATEGORY" }], page > 1);
+  }, [page, fetchProducts, selectedFilter]);
 
   const handleScroll = useCallback(() => {
     const scrollPosition = window.innerHeight + document.documentElement.scrollTop;
     const totalHeight = document.documentElement.offsetHeight;
     const seventyPercentHeight = 0.9 * totalHeight;
-    if (
-      scrollPosition >= seventyPercentHeight && !loading
-    ) {
+    if (scrollPosition >= seventyPercentHeight && !loading) {
       setPage((prevPage) => prevPage + 1);
     }
   }, [loading]);
@@ -156,21 +154,28 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
           throw new Error("Network response was not ok");
         }
         const responseData = await response.json();
-        const data = responseData?.["data"]?.["getListingFilters"]?.["easyFilters"]
+        let data = responseData?.["data"]?.["getListingFilters"]?.["easyFilters"];
+        const allFilter = {
+          name: "All",
+          uniqueId: "all",
+          contentType: "CATEGORY",
+        };
+        data = [allFilter, ...data];
         setFilteredData(data);
-        setSelectedFilter(data[0].uniqueId)
+        setSelectedFilter(data[0].uniqueId);
       } catch (error) {
         console.error("Error fetching filters:", error);
       }
-
-
     };
 
     fetchFilters();
   }, []);
 
-  const handleFilterClick = (uniqueId: any) => {
+  const handleFilterClick = async (uniqueId: string) => {
     setSelectedFilter(uniqueId);
+    setPage(1);
+    const filters = uniqueId === "all" ? [] : [{ id: uniqueId, type: "CATEGORY" }];
+    await fetchProducts(1, filters, false);
   };
 
   const router = useRouter();
@@ -179,46 +184,41 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
   const { Product, setProduct } = contextValue;
 
   const redirectToProductDetail = (product: any) => {
-    setProduct(product)
+    setProduct(product);
     if (typeof window !== 'undefined') {
       localStorage.setItem("selectedProduct", JSON.stringify(product));
     }
-    
-    
     router.push(`/productDetail?id=${product.id}`);
-
-  }
+  };
 
   return (
     <div>
       <div>
         <img src={bannerImage} className={styles.bannerImage} alt="Banner Image" />
-
       </div>
       {products?.length > 0 && (
         <div className={styles.productsCountTextContainer}>
           <p className={styles.productsCountTextContainerText}>Shop Products</p>
           <div className={styles.productsCountTextContainerDot}></div>
-          <p
-            className={
-              styles.productsCountTextContainerCount
-            }>{`${totalProdcuts} Products`}</p>
+          <p className={styles.productsCountTextContainerCount}>
+            {`${totalProdcuts} Products`}
+          </p>
         </div>
       )}
       <div>
-
         <FilterList filters={filteredData} selectedFilter={selectedFilter} onFilterClick={handleFilterClick} />
       </div>
       <div className={styles.productsListContainer}>
         {products.map((each, index) => (
           <div
             key={index}
-            className={`${styles.Productcontainer} ${(index - 2) % 5 === 0 ? "width-98" : "width-48"
-              }`} onClick={() => redirectToProductDetail(each)}>
+            className={`${styles.Productcontainer} ${(index - 2) % 5 === 0 ? "width-98" : "width-48"}`}
+            onClick={() => redirectToProductDetail(each)}
+          >
             <div
               style={{ backgroundImage: `url(${each.images[0].src})` }}
-              className={`${styles.productImage} ${(index - 2) % 5 === 0 ? "height-300" : "height-181"
-                }`}>
+              className={`${styles.productImage} ${(index - 2) % 5 === 0 ? "height-300" : "height-181"}`}
+            >
               <div onClick={(event) => event.stopPropagation()}>
                 <ShareButton
                   shareData={{
@@ -229,20 +229,12 @@ const ProductList: FC<ProductListProps> = ({ title }) => {
               </div>
             </div>
             <div className={styles.productListContent}>
-              <p className={styles.productListContentTitle}>
-                {each.brand[0].name}
-              </p>
+              <p className={styles.productListContentTitle}>{each.brand[0].name}</p>
               <p className={styles.productListContentName}>{each.title}</p>
               <div className={styles.productListContentPricingContainer}>
                 {`Rs. ${each.price.value}`}
-                <span
-                  className={
-                    styles.productListContentMRP
-                  }>{`Rs. ${each.MRP.value}`}</span>
-                <p
-                  className={
-                    styles.productListContentDiscount
-                  }>{`${each.discountPercent}%`}</p>
+                <span className={styles.productListContentMRP}>{`Rs. ${each.MRP.value}`}</span>
+                <p className={styles.productListContentDiscount}>{`${each.discountPercent}%`}</p>
               </div>
             </div>
           </div>
